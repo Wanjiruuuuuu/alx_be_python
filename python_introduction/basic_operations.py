ba578446d3865dfd50ef1afac4ecc4fd1b16d5f0
Rachel <rachelwanjiru037@gmail.com>
2024-12-13 00:50:12 +0300

    Add basic_operations.py for python arithmetic task

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#Assigning values to the variables 
+
+number1 = 10
+number2 = 5
+
+# Performing arithmetic operations
+
+addition = number1 + number2
+print (f"Addition of {number1} and {number2} is", {addition})
+
+subtraction = number1 - number2
+print (f"Subtraction of {number1} and {number2} is", {subtraction})
+
+multiplication = number1 * number2
+print (f"Multiplication of {number1} and {number2} is", {multiplication})
