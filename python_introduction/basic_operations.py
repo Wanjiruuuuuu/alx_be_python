0bb7fbf559eaffc91c74060dc08f22d810fa1528
Rachel <rachelwanjiru037@gmail.com>
2024-12-14 15:41:02 +0300

    Update code

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -6,10 +6,10 @@ number2 = 5
 # Performing arithmetic operations
 
 addition = number1 + number2
-print (f"Addition of {number1} and {number2} is", {addition})
+print(f"Addition of {number1} and {number2} is {addition}")
 
 subtraction = number1 - number2
-print (f"Subtraction of {number1} and {number2} is", {subtraction})
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
 
 multiplication = number1 * number2
-print (f"Multiplication of {number1} and {number2} is", {multiplication})
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
