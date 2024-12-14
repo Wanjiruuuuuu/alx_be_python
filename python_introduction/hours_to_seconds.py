44ceadfc35b80871125132b4d698fe5fdc497385
Rachel <rachelwanjiru037@gmail.com>
2024-12-14 17:44:53 +0300

    Hours into seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,12 @@
+#calculating hours to seconds
+#Defining the variable hours 
+
+hours = 2 
+
+#calculating number of seconds in 2 hours 
+
+seconds = hours * 3600
+
+#Print result
+
+print(f"{hours} hours(s) is {seconds} seconds." )
