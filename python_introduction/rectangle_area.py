6aeb8c050967b11df2cf71d99ae10402d12d9ba9
Rachel <rachelwanjiru037@gmail.com>
2024-12-14 17:26:21 +0300

    Correct variable spelling

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,11 +1,11 @@
 # declaring the values of the measurements of the rectangle
 
-lengh = 10
+length = 10
 width = 5
 
 #calculating the area of the rectangle
 
-area = lengh * width
+area = length * width
 
 print("The area of the rectangle is:", area)
 
