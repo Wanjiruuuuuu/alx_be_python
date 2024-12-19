5521a5ed8105da16c95d9c8a6aa0f44d02bc4aee
Rachel <rachelwanjiru037@gmail.com>
2024-12-19 19:59:05 +0300

    Added control-flow.

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,17 @@
+#Program that provieds clothing recommendations according to the given weather
+#Promt user for the weatheer input
+weather = (input("What's the weather like today?,(sunny/rainy/cold): ")).lower()
+
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+
+elif weather == "rainy":
+    print("Don't forget your umbrella and raincoat.")
+
+elif weather == "cold":
+    print("Make sure you wear a warm coat and a scarf. ")
+
+else :
+    print("Sorry, I dont have recommendations for this weather.")
+
+
