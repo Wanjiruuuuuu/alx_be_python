d588597d7c02021c34c0ec671a1e6fedbff0dce0
Rachel <rachelwanjiru037@gmail.com>
2024-12-22 11:01:43 +0300

    Pattern drawing

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,26 @@
+
+
+# User input postive interger
+
+size = int(input("Enter the size of the pattern:"))
+
+#initialize row counter
+row = 0 
+
+#while loop to iterate through each row
+while row < size:
+    for _ in range(size):
+        print("*",end="")
+
+    print()
+
+    row += 1 
+
+
+
+
+
+
+
+
+
