c31912c3910a03537f3ddd219d026dba18149804
Rachel <rachelwanjiru037@gmail.com>
2024-12-23 08:16:04 +0300

    Updtaed using print

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -10,32 +10,37 @@ time_bound = input("Is it time-bound? (yes/no): ").lower()
 # Process task with match case
 match priority:
     case "high":
-        reminder = f"Reminder: '{task}' is a high priority task."
+        reminder = f"'{task}' is a high priority task."
+        
     case "medium":
-        reminder = f"Reminder: '{task}' is a medium priority task."
+        reminder = f"'{task}' is a medium priority task."
+        
     case "low":
-        note = f"Note: '{task}' is a low priority task."
+        note = f"'{task}' is a low priority task."
+        
     case _:
         reminder = "Invalid priority level entered."
+        
 
-# Add time sensitivity if applicable
+# Add time sensitivity if applicables
 if priority == "low":
     if time_bound == "yes":
-        reminder += " that requires immediate attention today!"
+        note += " that requires immediate attention today!"
+        print("\n Note:", note)
     elif time_bound == "no":
         note += " Consider completing it when you have free time."
+        print("\nNote:", note)
     else:
         note += " Time sensitivity was not specified correctly."
+        print("\nNote:", note)
 else:
     if time_bound == "yes":
         reminder += " that requires immediate attention today!"
+        print("\nReminder:", reminder)
     elif time_bound == "no":
-        note += " Consider completing it when you have free time."
+        reminder += " Consider completing it when you have free time."
+        print("\nReminder:", reminder)
     else:
         reminder += " Time sensitivity was not specified correctly."
+        print("\nReminder:", reminder)
 
-# Print the customized reminder or note
-if time_bound == "low":
-    print("\n", note)
-else:
-    print("\n", reminder)
